f219ad99035bcd251efec3c0e58cb1fd8ec390e8
Simple <simple.ma@aax.com>
2023-01-01 19:00:42 +0800

    feat():add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('myh mestJs API doc')
+    .setDescription('nestjs project document')
+    .setVersion('1.0')
+    .addBasicAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
