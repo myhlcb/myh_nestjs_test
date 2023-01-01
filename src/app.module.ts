cf2beb23edb7b3316ecd46f6a5cfde91f34b8e8c
S1mple <1029785965@qq.com>
2023-01-01 21:13:04 +0800

    feat():modules外部注入

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,20 @@ import { AppService } from './services/app.service';
 import { HelloModule } from './modules/hello.module';
 import { CatModule } from './modules/cat.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+
+import { ConfigModule, ConfigService } from 'nestjs-config';
+import { resolve } from 'path';
+import { MailerModule } from '@nestjs-modules/mailer';
 @Module({
-  imports: [HelloModule, CatModule],
+  imports: [
+    // ConfigModule.load(resolve(__dirname,'config','xxx')), 注入config
+    MailerModule.forRootAsync({
+      // useFactory: (config: ConfigService) => config.get('email'),
+      // inject: [ConfigService],  //modules模块外部注入
+    }),
+    HelloModule,
+    CatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
